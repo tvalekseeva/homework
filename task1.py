89bd52428c5790dfde21ba9ae381332dc9e20a50
tvalekseeva <sibcats@gmail.com>
2021-11-07 23:32:03 +0300

    Код теста

---
+++ task1.py
@@ -0,0 +1,14 @@
+"""
+Простой пример программы
+Рассчет чисел Фиббоначи
+"""
+n = 10
+a = 0
+b = 1
+f = [a]
+for i in range(n):
+    temp = a
+    a = b
+    b += temp
+    f.append(a)
+print("Fibonacci number for {} is {}".format(n, a))
